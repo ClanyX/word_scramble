e7238850bb69875097df36db9f2ca56c304b831c
Filip <101202559+ClanyX@users.noreply.github.com>
2025-01-26 18:00:12 +0100

    start the game, making UI and some basic script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { Button } from "@sveltestrap/sveltestrap";
+
+    const alfabet = "abcdefghijklmnopqrstuvwxyz";
+    const alfabetArray = alfabet.split("");
+
+    let word = "Word";
+
+    function getWord() {
+        let randomLetter = alfabetArray[Math.floor(Math.random() * alfabetArray.length)];
+        fetch(`https://api.datamuse.com/words?sp=${randomLetter}*`)
+            .then((response) => response.json())
+            .then((values) => {
+                const randomWord = values[Math.floor(Math.random() * values.length)].word;
+                word = randomWord;
+            })
+            .catch((error) => console.error(error));
+    }
+
+</script>
+
+<div class="d-flex justify-content-center align-items-center flex-column" style="height: 70vh;">
+    <div id="outputText">{word.toUpperCase()}</div>
+    <input type="text" id="inputText" /><br />
+    <button on:click={getWord}>Submit</button>
+</div>
+
+<style>
+    #outputText {
+        font-size: 10rem;
+    }
+</style>
