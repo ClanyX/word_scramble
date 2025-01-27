d4615ae77806115963491baf77a27dc1ce60d37a
Filip <101202559+ClanyX@users.noreply.github.com>
2025-01-27 19:01:15 +0100

    can change definition but can go lower than 0 and higher than definition.length

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { error } from "@sveltejs/kit";
     import { Button } from "@sveltestrap/sveltestrap";
     import { get } from "svelte/store";
 
@@ -6,34 +7,67 @@
     const alfabetArray = alfabet.split("");
 
     let letters = getWord();
-    let definition;
+    let definition = [];
+
+    let score = 0;
 
     function getWord() {
         //get random word
         let randomLetter = alfabetArray[Math.floor(Math.random() * alfabetArray.length)];
         fetch(`https://api.datamuse.com/words?sp=${randomLetter}*`)
-            .then((response) => response.json())
+            .then((response) => {
+                if(!response.ok){
+                    throw new Error('Network response was not ok. Status: ' + response.status);
+                }
+                return response.json();
+            })
             .then((values) => {
                 const randomWord = values[Math.floor(Math.random() * values.length)].word;
                 letters = randomWord.toUpperCase().split('');
+                getDefinition();
                 //delete this
                 console.log(letters);
-                //get definition of the word
-                fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${letters.join('')}`)
-                    .then((response) => response.json())
-                    .then((data) => {
-                        definition = data[0].meanings[0].definitions[0].definition;
-                    })
-                    .catch((error) => console.error(error));
             })
-            .catch((error) => console.error(error));
+            .catch  (error => {console.error('Error: ', error)});
     }
+
+    function getDefinition(){
+        //get definition of the word
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${letters.join('')}`)
+            .then(response => {
+                if(!response.ok){
+                    throw new Error('Network response was not ok Status: ' + response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                definition = data[0].meanings[0].definitions;
+                prepareDefinition();
+                changeDefinition(definitionCount);
+            })
+            .catch(error => {console.error('Error: ', error)});
+    }
+
+    function prepareDefinition(){
+        for(let i = 0; i < definition.length; i++){
+            definition[i] = definition[i].definition;
+        }
+    }
+
+    //change the definition
+    let definitionCount = 0;
+    let definitionExport;
+    
+    function changeDefinition(num){
+        definitionExport = definition[num];
+    }
+    $: changeDefinition(definitionCount);
 
     function scanWord(){
         const inputs = document.querySelectorAll('input');
         let index = 0;
-        //check if one input is correct
         inputs.forEach(input => {
+            //check if one input is correct
             if(input.value.toUpperCase() === letters[index]){
                 input.style.backgroundColor = 'green';
                 input.readOnly = true;
@@ -49,6 +83,7 @@
                 resetForm();
                 getWord();
                 alert('You won! Correct word is: ' + letters.join(''));
+                score++;
             }
             index++;
         });
@@ -65,14 +100,20 @@
 </script>
 
 <div>
-    <form id="form" class="d-flex justify-content-center align-items-center flex-column" style="height: 100vh;">
-        <div>{definition}</div>
+    <h1 class="text-center mt-5">Guess the word</h1>
+    <div>Your score: {score}</div>
+    <form id="form" class="d-flex justify-content-center align-items-center flex-column" style="height: 60vh;">
+        <div class="justify-content-center align-items-center">
+            <button class="btn btn-secondary" onclick={() => definitionCount--}>←</button>
+            {definitionExport}
+            <button class="btn btn-secondary" onclick={() => definitionCount++}>→</button>
+        </div>
         <div class="outputText mb-5">
             {#each letters as letter}
                 <input type="text" maxlength="1"/>
             {/each}
         </div>
-        <button id="resetForm" class="btn btn-secondary" on:click={scanWord}>Submit</button>
+        <button id="resetForm" class="btn btn-secondary" onclick={scanWord}>Submit</button>
     </form>
 </div>
 
